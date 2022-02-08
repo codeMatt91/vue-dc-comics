c92ae97d08f5917588d3fbc11ddf953aeb45ebcd
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-08 16:09:16 +0100

    Added file sass

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,24 +1,26 @@
 <template>
   <header>
-    <div>
-      <figure>
-        <a href="#"><img src="../assets/img/dc-logo.png" alt="DC logo" /></a>
-      </figure>
+    <div class="container">
+      <div id="logo">
+        <figure>
+          <a href="#"><img src="../assets/img/dc-logo.png" alt="DC logo" /></a>
+        </figure>
+      </div>
+      <nav>
+        <ul>
+          <li><a href="#">CHARACTERS</a></li>
+          <li><a href="#">COMICS</a></li>
+          <li><a href="#"> MOVIES</a></li>
+          <li><a href="#">TV</a></li>
+          <li><a href="#">GAMES</a></li>
+          <li><a href="#">COLLECTIBLES</a></li>
+          <li><a href="#">VIDEOS</a></li>
+          <li><a href="#">FANS</a></li>
+          <li><a href="#">NEWS</a></li>
+          <li><a href="#">SHOP</a></li>
+        </ul>
+      </nav>
     </div>
-    <nav>
-      <ul>
-        <li><a href="#">CHARACTERS</a></li>
-        <li><a href="#">COMICS</a></li>
-        <li><a href="#"> MOVIES</a></li>
-        <li><a href="#">TV</a></li>
-        <li><a href="#">GAMES</a></li>
-        <li><a href="#">COLLECTIBLES</a></li>
-        <li><a href="#">VIDEOS</a></li>
-        <li><a href="#">FANS</a></li>
-        <li><a href="#">NEWS</a></li>
-        <li><a href="#">SHOP</a></li>
-      </ul>
-    </nav>
   </header>
 </template>
 
@@ -30,22 +32,32 @@ export default {
 
 <style lang="scss" served>
 @import "../assets/sass/_vars.scss";
+@import "../assets/sass/_utils.scss";
 
-header {
+.container {
   display: flex;
   justify-content: space-between;
 }
 nav {
   display: flex;
-  align-items: center;
+
   ul {
     display: flex;
+    height: $h-100;
     li {
       list-style-type: none;
+      height: $h-100;
+      display: flex;
+      align-items: center;
       a {
+        font-size: 0.8rem;
         text-decoration: none;
         color: $c-black;
         padding: 5px;
+        margin: 5px;
+        height: $h-100;
+        display: flex;
+        align-items: center;
       }
     }
   }
