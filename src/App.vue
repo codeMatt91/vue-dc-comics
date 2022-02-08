3fe17323e952dccd73c219096f9837e73081a187
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-08 17:20:44 +0100

    Added Main + style

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div>
     <Header />
-    <main></main>
+    <Main />
     <footer></footer>
   </div>
 </template>
 
 <script>
 import Header from "./components/Header";
+import Main from "./components/Main";
+
 export default {
   name: "App",
   components: {
     Header,
+    Main,
   },
 };
 </script>
---
+++ src/components/Main.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+    <div class="content">
+      <div class="container">
+        <h2>-- Content goes here --</h2>
+      </div>
+    </div>
+    <section id="shop">
+      <div class="container">
+        <div v-for="card in cards" :key="card.text" class="card-img">
+          <figure>
+            <img :src="`../assets/img/${card.url}`" :alt="card.description" />
+            <div>{{ card.text }}</div>
+          </figure>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+export default {
+  name: "Main",
+  data() {
+    return {
+      cards: [
+        {
+          text: "DIGITAL COMICS",
+          url: "buy-comics-digital-comics.png",
+          description: "logo dc",
+        },
+        {
+          text: "DC MERCHANDISE",
+          url: "buy-comics-merchandise.png",
+          description: "logo merchandise",
+        },
+        {
+          text: "SUBSCRIPTION",
+          url: "buy-comics-subscriptions.png",
+          description: "logo subscriptions",
+        },
+        {
+          text: "COMIC SHOP LOCATOR",
+          url: "buy-comics-shop-locator.png",
+          description: "logo shop",
+        },
+        {
+          text: "DC POWER VISA",
+          url: "buy-dc-power-visa.svg",
+          description: "logo visa",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style lang="scss" served>
+@import "../assets/sass/_vars.scss";
+
+.content {
+  background-color: $c-black;
+  height: 150px;
+  display: flex;
+  align-items: center;
+}
+h2 {
+  color: $c-white;
+}
+
+#shop {
+  color: $c-white;
+  min-height: 150px;
+  background-color: $c-blue;
+}
+
+.card-img figure {
+  display: flex;
+  align-items: center;
+}
+</style>
