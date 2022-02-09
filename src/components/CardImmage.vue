ba82533a040dbfb013a218643e9534e7c5d46593
codeMatt91 <matteo.imbimbo@yahoo.it>
2022-02-09 16:06:30 +0100

    Added CardImmage component + moved data in APP.vue

--- src/components/CardImmage.vue
+++ src/components/CardImmage.vue
@@ -1,15 +1,26 @@
 <template>
-  <div>
-
+  <div class="card-serie">
+    <img :src="thumb" :alt="type" />
+    <h5>{{ series.toUpperCase() }}</h5>
   </div>
 </template>
 
 <script>
 export default {
-name: "CardImmage"
-}
+  name: "CardImmage",
+  props: ["thumb", "series", "type"],
+};
 </script>
 
 <style lang="scss" scoped>
+@import "../assets/sass/_vars.scss";
 
+.card-serie {
+  flex-basis: calc(100% / 6);
+  color: $c-white;
+  padding-bottom: 20px;
+  .img {
+    height: 50%;
+  }
+}
 </style>
